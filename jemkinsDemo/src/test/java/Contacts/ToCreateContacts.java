eb4c8afabdcb066654f75275302d16d89a634e2b
UMA MAHESH <UMA MAHESH@LAPTOP-3RU2HEOP>
2024-11-13 08:29:59 +0530

    pushed

---
+++ jemkinsDemo/src/test/java/Contacts/ToCreateContacts.java
@@ -0,0 +1,15 @@
+package Contacts;
+
+import java.sql.Driver;
+
+public class ToCreateContacts {
+
+	public static void main(String[] args) {
+
+		System.out.println("contacts created successfully");
+		
+		System.out.println(" Contacts updated Successfully");
+
+	}
+
+}
---
+++ jemkinsDemo/src/test/java/Oraganization/ToCreateOrganization.java
@@ -0,0 +1,12 @@
+package Oraganization;
+
+public class ToCreateOrganization {
+
+	public static void main(String[] args) {
+
+		System.out.println("Oraganization created successfully");
+		System.out.println("Organisation details updated successfully");
+
+	}
+
+}
